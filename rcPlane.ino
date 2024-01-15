ea7972243c08178bfa12b445c9909f0fe40102db
jusbro <97056196+jusbro@users.noreply.github.com>
2024-01-15 18:53:06 -0500

    Create rcPlane.ino

---
+++ rcPlane.ino
@@ -0,0 +1,260 @@
+#include <Bluepad32.h>
+#include <Servo.h>
+#include <pico/multicore.h>
+
+
+#define GRN_LED 10
+#define LED_PIN 13
+#define SERVO_PIN A3
+ControllerPtr myControllers[BP32_MAX_CONTROLLERS];
+Servo myservo;
+
+// Arduino setup function. Runs in CPU 1
+void setup() {
+  // Initialize serial
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(SERVO_PIN, OUTPUT);
+  pinMode(GRN_LED, OUTPUT);
+  myservo.attach(SERVO_PIN);
+  sleep_ms(5000);
+  multicore_launch_core1(loop1);
+  sleep_ms(500);
+
+  while (!Serial) {
+    // wait for serial port to connect.
+    // You don't have to do this in your game. This is only for debugging
+    // purposes, so that you can see the output in the serial console.
+    ;
+  }
+
+  String fv = BP32.firmwareVersion();
+  Serial.print("Firmware version installed: ");
+  Serial.println(fv);
+
+  // To get the BD Address (MAC address) call:
+  const uint8_t* addr = BP32.localBdAddress();
+  Serial.print("BD Address: ");
+  for (int i = 0; i < 6; i++) {
+    Serial.print(addr[i], HEX);
+    if (i < 5)
+      Serial.print(":");
+    else
+      Serial.println();
+  }
+
+  // BP32.pinMode(27, OUTPUT);
+  // BP32.digitalWrite(27, 0);
+
+  // This call is mandatory. It setups Bluepad32 and creates the callbacks.
+  BP32.setup(&onConnectedController, &onDisconnectedController);
+
+  // "forgetBluetoothKeys()" should be called when the user performs
+  // a "device factory reset", or similar.
+  // Calling "forgetBluetoothKeys" in setup() just as an example.
+  // Forgetting Bluetooth keys prevents "paired" gamepads to reconnect.
+  // But might also fix some connection / re-connection issues.
+  BP32.forgetBluetoothKeys();
+}
+
+// This callback gets called any time a new gamepad is connected.
+// Up to 4 gamepads can be connected at the same time.
+void onConnectedController(ControllerPtr ctl) {
+  bool foundEmptySlot = false;
+  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+    if (myControllers[i] == nullptr) {
+      Serial.print("CALLBACK: Controller is connected, index=");
+      Serial.println(i);
+      myControllers[i] = ctl;
+      foundEmptySlot = true;
+
+      // Optional, once the gamepad is connected, request further info about the
+      // gamepad.
+      ControllerProperties properties = ctl->getProperties();
+      char buf[80];
+      sprintf(buf,
+              "BTAddr: %02x:%02x:%02x:%02x:%02x:%02x, VID/PID: %04x:%04x, "
+              "flags: 0x%02x",
+              properties.btaddr[0], properties.btaddr[1], properties.btaddr[2],
+              properties.btaddr[3], properties.btaddr[4], properties.btaddr[5],
+              properties.vendor_id, properties.product_id, properties.flags);
+      Serial.println(buf);
+      break;
+    }
+  }
+  if (!foundEmptySlot) {
+    Serial.println(
+        "CALLBACK: Controller connected, but could not found empty slot");
+  }
+}
+
+void onDisconnectedController(ControllerPtr ctl) {
+  bool foundGamepad = false;
+
+  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+    if (myControllers[i] == ctl) {
+      Serial.print("CALLBACK: Controller is disconnected from index=");
+      Serial.println(i);
+      myControllers[i] = nullptr;
+      foundGamepad = true;
+      break;
+    }
+  }
+
+  if (!foundGamepad) {
+    Serial.println(
+        "CALLBACK: Controller disconnected, but not found in myControllers");
+  }
+}
+
+void processGamepad(ControllerPtr gamepad) {
+  // There are different ways to query whether a button is pressed.
+  // By query each button individually:
+  //  a(), b(), x(), y(), l1(), etc...
+
+  if (gamepad->a()) {
+    static int colorIdx = 0;
+    // Some gamepads like DS4 and DualSense support changing the color LED.
+    // It is possible to change it by calling:
+    switch (colorIdx % 3) {
+      case 0:
+        // Red
+        gamepad->setColorLED(255, 0, 0);
+        break;
+      case 1:
+        // Green
+        gamepad->setColorLED(0, 255, 0);
+        break;
+      case 2:
+        // Blue
+        gamepad->setColorLED(0, 0, 255);
+        break;
+    }
+    colorIdx++;
+  }
+
+  if (gamepad->b()) {
+    // Turn on the 4 LED. Each bit represents one LED.
+    static int led = 0;
+    led++;
+    // Some gamepads like the DS3, DualSense, Nintendo Wii, Nintendo Switch
+    // support changing the "Player LEDs": those 4 LEDs that usually indicate
+    // the "gamepad seat".
+    // It is possible to change them by calling:
+    gamepad->setPlayerLEDs(led & 0x0f);
+  }
+
+  if (gamepad->x()) {
+    // Set rumble for 1 second with moderate intensity
+    gamepad->setRumble(0x80 /* force */, 255 /* duration */);
+    sleep_ms(1000);  // Delay for 1 second to allow rumble effect
+    gamepad->setRumble(0x00 /* force */, 0 /* duration */);  // Stop the rumble
+}
+
+
+  // Another way to query the buttons, is by calling buttons(), or
+  // miscButtons() which return a bitmask.
+  // Some gamepads also have DPAD, axis and more.
+  char buf[256];
+  snprintf(buf, sizeof(buf) - 1,
+           "idx=%d, dpad: 0x%02x, buttons: 0x%04x, "
+           "axis L: %4li, %4li, axis R: %4li, %4li, "
+           "brake: %4ld, throttle: %4li, misc: 0x%02x, "
+           "gyro x:%6d y:%6d z:%6d, accel x:%6d y:%6d z:%6d, "
+           "battery: %d",
+           gamepad->index(),        // Gamepad Index
+           gamepad->dpad(),         // DPAD
+           gamepad->buttons(),      // bitmask of pressed buttons
+           gamepad->axisX(),        // (-511 - 512) left X Axis
+           gamepad->axisY(),        // (-511 - 512) left Y axis
+           gamepad->axisRX(),       // (-511 - 512) right X axis
+           gamepad->axisRY(),       // (-511 - 512) right Y axis
+           gamepad->brake(),        // (0 - 1023): brake button
+           gamepad->throttle(),     // (0 - 1023): throttle (AKA gas) button
+           gamepad->miscButtons(),  // bitmak of pressed "misc" buttons
+           gamepad->gyroX(),      // Gyro X
+           gamepad->gyroY(),      // Gyro Y
+           gamepad->gyroZ(),      // Gyro Z
+           gamepad->accelX(),     // Accelerometer X
+           gamepad->accelY(),     // Accelerometer Y
+           gamepad->accelZ(),     // Accelerometer Z
+           gamepad->battery()       // 0=Unknown, 1=empty, 255=full
+
+  );
+  Serial.println(buf);
+  int count = gamepad->throttle();
+  int servoCount = gamepad->axisX();
+  int servoPos = map(servoCount, -511, 511, 0, 180);
+  myservo.write(servoPos);
+  int brightness = map(count, 0, 1023, 0, 255);
+  analogWrite(LED_PIN, brightness);
+
+  // You can query the axis and other properties as well. See
+  // Controller.h For all the available functions.
+}
+
+void processMouse(ControllerPtr mouse) {
+  char buf[160];
+  sprintf(buf,
+          "idx=%d, deltaX:%4li, deltaY:%4li, buttons: 0x%04x, misc: 0x%02x, "
+          "scrollWheel: %d, battery=%d",
+          mouse->index(),        // Controller Index
+          mouse->deltaX(),       // Mouse delta X
+          mouse->deltaY(),       // Mouse delta Y
+          mouse->buttons(),      // bitmask of pressed buttons
+          mouse->miscButtons(),  // bitmak of pressed "misc" buttons
+          mouse->scrollWheel(),  // Direction: 1=up, -1=down, 0=no movement
+          mouse->battery()       // 0=Unk, 1=Empty, 255=full
+  );
+  Serial.println(buf);
+}
+
+void processBalanceBoard(ControllerPtr balance) {
+  char buf[160];
+  sprintf(buf,
+          "idx=%d, tl:%4i, tr:%4i, bl: %4i, br: %4i, temperature=%d, "
+          "battery=%d",
+          balance->index(),  // Controller Index
+          balance->topLeft(), balance->topRight(), balance->bottomLeft(),
+          balance->bottomRight(), balance->temperature(),
+          balance->battery()  // 0=Unk, 1=Empty, 255=full
+  );
+  Serial.println(buf);
+}
+
+// Arduino loop function. Runs in CPU 1
+void loop() {
+  // This call fetches all the controller info from the NINA (ESP32) module.
+  // Just call this function in your main loop.
+  // The controllers pointer (the ones received in the callbacks) gets updated
+  // automatically.
+  BP32.update();
+
+  // It is safe to always do this before using the controller API.
+  // This guarantees that the controller is valid and connected.
+  for (int i = 0; i < BP32_MAX_CONTROLLERS; i++) {
+    ControllerPtr myController = myControllers[i];
+
+    if (myController && myController->isConnected()) {
+      if (myController->isGamepad())
+        processGamepad(myController);
+      else if (myController->isMouse())
+        processMouse(myController);
+      else if (myController->isBalanceBoard())
+        processBalanceBoard(myController);
+    }
+  }
+  sleep_ms(150);
+  //digitalWrite(LED_PIN, HIGH);
+  //delay(500);
+  //digitalWrite(LED_PIN, LOW);
+}
+
+void loop1(){
+  while(true){
+    digitalWrite(GRN_LED, LOW);
+    sleep_ms(600);
+    digitalWrite(GRN_LED, HIGH);
+    sleep_ms(600);
+}
+}
